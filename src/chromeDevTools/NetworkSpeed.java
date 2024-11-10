9d98b511d2d2408450ba2862a369daec3cdd9472
CodeNinja0615 <sameerakhtar1513@gmail.com>
2024-11-11 04:35:31 +0530

    Network speed and loading failure test with CDP, Basic-Auth Demo and ConsoleLogCapture Demo

---
+++ src/chromeDevTools/NetworkSpeed.java
@@ -0,0 +1,34 @@
+package chromeDevTools;
+
+import java.time.Duration;
+import java.util.Optional;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v128.network.Network;
+import org.openqa.selenium.devtools.v128.network.model.ConnectionType;
+
+public class NetworkSpeed {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		DevTools devTools = driver.getDevTools();
+		devTools.createSession();
+		devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+		devTools.send(Network.emulateNetworkConditions(true, 3000, 20000, 10000, Optional.of(ConnectionType.ETHERNET),
+				Optional.empty(), Optional.empty(), Optional.empty()));
+		
+		devTools.addListener(Network.loadingFailed(), loadingFailed -> { //---Works only on loading failure
+			System.out.println(loadingFailed.getErrorText());
+			System.out.println(loadingFailed.getTimestamp());
+		});
+		
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.cssSelector("[routerlink*='library']")).click();
+
+	}
+
+}
