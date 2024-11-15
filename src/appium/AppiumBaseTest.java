0e62858efd4c73ac898dceefc1f82d58c279d6f1
CodeNinja0615 <sameerakhtar1513@gmail.com>
2024-11-16 02:51:08 +0530

    Added BaseTest for Appium setup and TearDown of driver and services

---
+++ src/appium/AppiumBaseTest.java
@@ -0,0 +1,45 @@
+package appium;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+public class AppiumBaseTest {
+
+	public AppiumDriverLocalService service;
+	public AndroidDriver driver;
+	
+	@BeforeMethod
+	public void setUp() throws MalformedURLException, URISyntaxException {
+		service = new AppiumServiceBuilder()
+				.withAppiumJS(
+						new File("C://Users//HP//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).build();
+		service.start();
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setDeviceName("ZA222JVBYL");
+//		options.setApp("C:/Users/HP/Downloads/General-Store.apk");
+//		adb shell dumpsys window | findstr "mCurrentFocus"
+//		options.setAppPackage("com.instagram.barcelona");
+//		options.setAppActivity("com.instagram.barcelona.mainactivity.BarcelonaActivity");
+		options.setNoReset(true);
+		driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+		service.stop();
+	}
+}
--- src/appium/AppiumBasics.java
+++ src/appium/AppiumBasics.java
@@ -1,37 +1,17 @@
 package appium;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
-
 import org.testng.annotations.Test;
+import io.appium.java_client.AppiumBy;
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
-
-public class AppiumBasics {
+public class AppiumBasics extends AppiumBaseTest {
 
 	@Test
 	public void appiumTest() throws MalformedURLException, URISyntaxException {
-		AppiumDriverLocalService service = new AppiumServiceBuilder()
-				.withAppiumJS(
-						new File("C://Users//HP//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
-				.withIPAddress("127.0.0.1").usingPort(4723).build();
-		service.start();
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName("ZA222JVBYL");
-//		options.setApp("C:/Users/HP/Downloads/General-Store.apk");
-//		adb shell dumpsys window | findstr "mCurrentFocus"
-//		options.setAppPackage("com.instagram.barcelona");
-//		options.setAppActivity("com.instagram.barcelona.mainactivity.BarcelonaActivity");
-		options.setNoReset(true);
-		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+		driver.unlockDevice();
 		driver.activateApp("com.instagram.barcelona");
+		driver.findElement(AppiumBy.xpath("//android.view.View[@resource-id=\"barcelona_tab_profile\"]/android.view.View[2]")).click();
 		driver.terminateApp("com.instagram.barcelona");
-		driver.close();
-		service.stop();
 	}
 }
