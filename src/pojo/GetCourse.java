0a124a648ba2bc7c052a7ec91aa83f2c6c326a06
Sameer <akhtarsameer743@gmail.com>
2025-03-18 01:41:54 +0530

    Added JSON in POJO class as example

--- src/pojo/GetCourse.java
+++ src/pojo/GetCourse.java
@@ -58,3 +58,46 @@ public class GetCourse {
 	}
 
 }
+
+
+
+
+//{
+//    "instructor": "RahulShetty",
+//    "url": "rahulshettycademy.com",
+//    "services": "projectSupport",
+//    "expertise": "Automation",
+//    "courses": {
+//        "webAutomation": [
+//            {
+//                "courseTitle": "Selenium Webdriver Java",
+//                "price": "50"
+//            },
+//            {
+//                "courseTitle": "Cypress",
+//                "price": "40"
+//            },
+//            {
+//                "courseTitle": "Protractor",
+//                "price": "40"
+//            }
+//        ],
+//        "api": [
+//            {
+//                "courseTitle": "Rest Assured Automation using Java",
+//                "price": "50"
+//            },
+//            {
+//                "courseTitle": "SoapUI Webservices testing",
+//                "price": "40"
+//            }
+//        ],
+//        "mobile": [
+//            {
+//                "courseTitle": "Appium-Mobile Automation using Java",
+//                "price": "50"
+//            }
+//        ]
+//    },
+//    "linkedIn": "https://www.linkedin.com/in/rahul-shetty-trainer/"
+//}
